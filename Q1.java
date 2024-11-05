70dc45718e33cb2dfd5c42d5acfb438b3a52b497
ARPI SAHA <mailofarpisaha@gmail.com>
2024-11-05 19:29:22 +0530

    Q1 added

---
+++ Q1.java
@@ -0,0 +1,16 @@
+/*Java program to print an integer number(Entered by the user) */
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+
+
+        System.out.println("Enter any number : ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+
+        System.out.println("The entered number is :"+ num);
+    }
+}
