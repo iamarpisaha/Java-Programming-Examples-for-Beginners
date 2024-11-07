2bc1c05674c8a1306ed7ddb7410be3feccb958fc
ARPI SAHA <mailofarpisaha@gmail.com>
2024-11-07 18:51:09 +0530

    Q4 added

--- Q4.java
+++ Q4.java
@@ -1,7 +1,16 @@
 /*Find ASCII value of a character*/
+class Ascii{
 
+    int getAsciiCode(char ch){
+        return (int) ch;
+    }
+}
 public class Q4 {
     public static void main(String[] args) {
+
+        char c = args[0].charAt(0);
+        Ascii c1 = new Ascii();
         
+        System.out.println("Ascii value of " + c + ":" + c1.getAsciiCode(c));
     }
 }
