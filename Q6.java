7e9c1a4c48d7f1e7dd9f098f8d03224a84305ba6
ARPI SAHA <mailofarpisaha@gmail.com>
2024-11-07 19:33:39 +0530

    Q6 added

---
+++ Q6.java
@@ -0,0 +1,17 @@
+/* Write a java program to print 2 numbers */
+
+import java.util.Scanner;
+
+public class Q6 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter First Number :");
+        int x = sc.nextInt();
+        System.out.println("Enter Second Number :");
+        int y = sc.nextInt();
+
+        System.out.println("First Number = " + x + " Second Number = " + y);
+    }
+}
